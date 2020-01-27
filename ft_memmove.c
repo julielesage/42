f2a9eda3f47e14eb828d13e398e08cb063180629
jlesage <57409607+julielesage@users.noreply.github.com>
2020-01-27 18:51:36 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -13,6 +13,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*La fonction memmove() copie n octets depuis la zone mémoire src vers la zone mémoire dest. 
+Les deux zones peuvent se chevaucher : la copie se passe comme si les octets de src étaient d'abord copiés dans une zone temporaire qui ne chevauche ni src ni dest, 
+et les octets sont ensuite copiés de la zone temporaire vers dest.  */
+
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	size_t			i;
@@ -37,3 +41,4 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	}
 	return (d);
 }
+*/
