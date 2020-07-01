878d4e2e391b424393d3c903687c6941eca0d043
jlesage <57409607+julielesage@users.noreply.github.com>
2020-07-01 17:25:37 +0200

    ➕ my_strcmp, out of libft

---
+++ my_strcmp.c
@@ -0,0 +1,14 @@
+int my_strcmp(char *s1, char *s2)
+{
+  if (s1 && s2)
+  {
+    while (*s1 == *s2 && *s1 && *s2)
+    {
+      s1++;
+      s2++;
+    }
+  }
+  else
+    return (1);
+  return (*s1 - *s2);
+}
